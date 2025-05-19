6129297a7353fa6bdc15e3fe0df06da613f49984
FameNu <110712106+FameNu@users.noreply.github.com>
2025-05-19 20:20:15 +0700

    add quest controller

---
+++ app/controllers/quests_controller.rb
@@ -0,0 +1,16 @@
+class QuestsController < ApplicationController
+  def index    
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def brag
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "quests#index"
+  get "/quests", to: redirect("/")
+  resources :quests, only: [:create, :update, :destroy]
+  get "/brag", to: "quests#brag"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
