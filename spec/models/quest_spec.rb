7b5fbb935ab16dfd91deb7055a20598e03faf1a5
FameNu <110712106+FameNu@users.noreply.github.com>
2025-06-15 21:06:35 +0700

    add: new test for Quest Model

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 describe Quest, type: :model do
   describe "default values" do
+    quest_title = "Test Quest"
+    quest = Quest.create!(title: quest_title)
+
+    it "has a title field" do
+      expect(quest.title).not_to be_nil
+      expect(quest.title).to eq(quest_title)
+    end
+
     it "has completed set to false by default" do
-      quest = Quest.create!(title: "Default test")
       expect(quest.completed).to eq(false)
     end
   end
